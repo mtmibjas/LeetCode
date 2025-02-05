d4eae0849b1237b1abb2e34787b6bf62928af3ab
Mohamed Ibjas <mtmibjas@gmail.com>
2025-02-05 17:28:43 +0530

    Time: 21 ms (71.23%), Space: 10.6 MB (15.59%) - LeetHub

--- 0015-3sum/0015-3sum.go
+++ 0015-3sum/0015-3sum.go
@@ -1,11 +1,5 @@
 func threeSum(nums []int) [][]int {
-	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i] > nums[j] {
-				nums[j], nums[i] = nums[i], nums[j]
-			}
-		}
-	}
+	sort.Ints(nums)
 
 	arr := make([][]int, 0)
 	for j := 0; j < len(nums)-2; j++ {
