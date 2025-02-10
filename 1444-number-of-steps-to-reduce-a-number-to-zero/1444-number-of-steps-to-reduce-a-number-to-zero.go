5f715cb22a60128baf8d1b6dd71511d6be5c98bb
Mohamed Ibjas <mtmibjas@gmail.com>
2025-02-10 23:33:26 +0530

    Time: 0 ms (100%), Space: 3.9 MB (78.59%) - LeetHub

--- 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.go
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.go
@@ -3,11 +3,12 @@ func numberOfSteps(num int) (count int) {
     for num != 0 {
         if num%2 == 0 {
             num = num/2
-            count++
+           
         }else{
             num = num - 1
-            count++
+            
         }
+         count++
     }
     return count 
 }
