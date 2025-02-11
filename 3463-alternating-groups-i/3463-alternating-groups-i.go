d5b5975facf23e0ab7662e1441546e35b467fc31
Mohamed Ibjas <mtmibjas@gmail.com>
2025-02-11 22:58:46 +0530

    Time: 8 ms (7.69%), Space: 4.6 MB (21.15%) - LeetHub

--- 3463-alternating-groups-i/3463-alternating-groups-i.go
+++ 3463-alternating-groups-i/3463-alternating-groups-i.go
@@ -1,19 +1,9 @@
 func numberOfAlternatingGroups(n []int) int {
     count := 0
     for i := 0; i < len(n); i++ {
-        if i > len(n)-2 {
-            if n[i] == n[1] && n[i] != n[0] {
-                count++
-            }
-        }else if i > len(n)-3 {
-            if n[i] == n[0] && n[i] != n[i+1] {
-                count++
-            }
-        }else{
-            if n[i] == n[i+2] && n[i] != n[i+1] {
-                count++
-            }
-        }
+         if n[(len(n)+i-1)%len(n)] != n[i] && n[i] != n[(i+1)%len(n)] {
+			count++
+		}
 
     } 
     return count
