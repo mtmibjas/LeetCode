67ad918cdb314eae2b8ab3d81e3d4881ce78ec52
Mohamed Ibjas <mtmibjas@gmail.com>
2025-02-14 10:00:59 +0530

    Time: 18 ms (57.27%), Space: 8.5 MB (50%) - LeetHub

---
+++ 0978-valid-mountain-array/0978-valid-mountain-array.go
@@ -0,0 +1,32 @@
+func validMountainArray(arr []int) bool {
+    if len(arr) < 2 {
+        return false
+    }
+    left := 0
+    right := len(arr)-1
+    move := right - left
+    for left < right {
+        if arr[left] == arr[left+1]|| arr[right] == arr[right-1] {
+            return false
+        }
+        if arr[left] < arr[left+1] {
+            left++
+        }
+      
+        if arr[right] < arr[right-1] {
+            right--
+        }
+        
+
+        if move == right - left{
+            return false
+        }
+        move = right -left
+
+    }
+    if left == 0 ||  right == len(arr)-1 {
+        return false
+    }
+    return true
+    
+}
