9bea3361952474c62a4ee60d8f04a99d65d28fe6
Mohamed Ibjas <mtmibjas@gmail.com>
2025-02-18 22:34:34 +0530

    Time: 0 ms (100%), Space: 8.1 MB (68.08%) - LeetHub

--- 0268-missing-number/0268-missing-number.go
+++ 0268-missing-number/0268-missing-number.go
@@ -1,9 +1,10 @@
 func missingNumber(nums []int) int {
     sort.Ints(nums)
+    if nums[len(nums)-1] != len(nums) {
+            return len(nums)
+    }
     for i := 0; i <= len(nums); i++ {
-       if i == len(nums) {
-            return i
-       }
+       
        if i != nums[i] {
             return i
        }
