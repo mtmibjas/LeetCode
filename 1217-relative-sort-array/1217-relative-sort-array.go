f10bc86b974c51722449dd587420897efc70e058
Mohamed Ibjas <mtmibjas@gmail.com>
2025-02-20 07:31:40 +0530

    Time: 1 ms (8.62%), Space: 4.4 MB (17.24%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.go
+++ 1217-relative-sort-array/1217-relative-sort-array.go
@@ -2,13 +2,10 @@ func relativeSortArray(arr1 []int, arr2 []int) []int {
 	arr := make([]int, 0)
     ars := make([]int, 0) 
 	m := make(map[int]int)
-    p := make(map[int]struct{}) 
-    for i := 0; i < len(arr2); i++ {
-		p[arr2[i]] = struct{}{}
-	}
+
 	for i := 0; i < len(arr1); i++ {
 		m[arr1[i]]++
-        if _, ok := p[arr1[i]]; !ok {
+        if !slices.Contains(arr2, arr1[i]){
             ars = append(ars, arr1[i])
         }
 	}
