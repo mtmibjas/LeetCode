f41eb15afa55db1df9a28c304010899323afc369
Mohamed Ibjas <mtmibjas@gmail.com>
2025-02-22 15:02:40 +0530

    Time: 3 ms (59.09%), Space: 8.8 MB (79.55%) - LeetHub

---
+++ 2881-split-strings-by-separator/2881-split-strings-by-separator.go
@@ -0,0 +1,21 @@
+func splitWordsBySeparator(words []string, separator byte) []string {
+	var arr []string
+	for _, w := range words {
+		s := 0
+
+		for i := 0; i < len(w); i++ {
+			if w[i] == separator {
+				if str := string(w[s:i]); str != "" {
+					arr = append(arr, str)
+				}
+				s = i + 1
+
+			}
+			if  str := string(w[s:]); str != "" && i == len(w)-1 {
+				arr = append(arr, str)
+			}
+		}
+	}
+
+	return arr
+}
