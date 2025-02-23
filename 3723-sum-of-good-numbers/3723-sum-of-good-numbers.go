9f4c824a4b3ea168ea1f491ab29bcaca8944a4d7
Mohamed Ibjas <mtmibjas@gmail.com>
2025-02-23 15:14:16 +0530

    Time: 0 ms (100%), Space: 5.1 MB (0.7%) - LeetHub

--- 3723-sum-of-good-numbers/3723-sum-of-good-numbers.go
+++ 3723-sum-of-good-numbers/3723-sum-of-good-numbers.go
@@ -1,21 +1,14 @@
 func sumOfGoodNumbers(nums []int, k int) int {
 	sum := 0
 	for i := 0; i < len(nums); i++ {
-		if i-k >= 0 && i+k <= len(nums)-1 {
-			if nums[i-k] < nums[i] && nums[i+k] < nums[i] {
-				sum += nums[i]
-			}
-		} else if i-k < 0 {
-			if nums[i+k] < nums[i] {
-				sum += nums[i]
-			}
-		} else if i+k > len(nums)-1 {
-			if nums[i-k] < nums[i] {
-				sum += nums[i]
-			}
-		}else{
+		if i-k >= 0 && nums[i-k] >= nums[i] {
+			continue
+		} 
+        if i+k < len(nums) && nums[i+k] >= nums[i] {
+			continue
+		}
             sum += nums[i]
-        }
+        
 	}
 
 	return sum
