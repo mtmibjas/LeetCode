dbde4e41a4467d551644ee90a28e6a98e6d6bc4b
Mohamed Ibjas <mtmibjas@gmail.com>
2025-02-23 06:40:20 +0530

    Time: 2 ms (82.76%), Space: 5.4 MB (48.77%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.go
+++ 1580-shuffle-the-array/1580-shuffle-the-array.go
@@ -2,9 +2,9 @@ func shuffle(nums []int, n int) []int {
     
     i := 0
     arr := make([]int, len(nums))
-    for i/2 < n {
-        arr[i],arr[i+1] = nums[i/2], nums[i/2+n]
-        i += 2
+    for i < n {
+        arr[i*2],arr[i*2+1] = nums[i], nums[i+n]
+        i++
     }
     return arr
 }
