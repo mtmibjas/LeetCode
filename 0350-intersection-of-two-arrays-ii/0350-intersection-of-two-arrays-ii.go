e05d871d0fb172d0cf5a0ca4e1bab0447dc96dfc
Mohamed Ibjas <mtmibjas@gmail.com>
2025-02-26 19:26:32 +0530

    Time: 0 ms (100%), Space: 4.6 MB (76.02%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.go
@@ -0,0 +1,16 @@
+func intersect(nums1 []int, nums2 []int) []int {
+    sharr, lnarr := nums1, nums2
+    if len(nums1) > len(nums2) {
+        sharr, lnarr = nums2, nums1
+    }
+    i := 0
+    arr := make([]int, 0)
+    for i < len(sharr){
+        if index := slices.Index(lnarr, sharr[i]); index != -1 {
+            arr = append(arr, sharr[i])
+            lnarr[index] = 9999
+        }
+        i++
+    }
+    return arr
+}
