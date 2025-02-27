824f01a615687bed3cfe808a3e17729f3adfe229
Mohamed Ibjas <mtmibjas@gmail.com>
2025-02-27 20:34:40 +0530

    Time: 0 ms (100%), Space: 4.8 MB (37.74%) - LeetHub

--- 3397-find-the-integer-added-to-array-i/3397-find-the-integer-added-to-array-i.go
+++ 3397-find-the-integer-added-to-array-i/3397-find-the-integer-added-to-array-i.go
@@ -1,5 +1,13 @@
 func addedInteger(nums1 []int, nums2 []int) int {
-    sort.Ints(nums1)
-    sort.Ints(nums2)
-    return nums2[0]-nums1[0]
+	max1 := 0
+	max2 := 0
+	for i := 0; i < len(nums1); i++ {
+        if max1 < nums1[i]{
+            max1 = nums1[i]
+        }
+        if max2 < nums2[i]{
+            max2 = nums2[i]
+        }
+	}
+    return max2-max1
 }
