42d493456abe1f37bcc1248a184b8df34171e5d6
Mohamed Ibjas <mtmibjas@gmail.com>
2025-03-03 23:54:01 +0530

    Time: 44 ms (19.53%), Space: 8.7 MB (59.17%) - LeetHub

--- 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.go
+++ 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.go
@@ -1,23 +1,17 @@
 func countConsistentStrings(allowed string, words []string) int {
-    arr := make([]string,0)
-    for i := 0; i < len(allowed); i++{
-        arr = append(arr, string(allowed[i]))
-    }
 
-    count := 0
-    for i := 0; i < len(words); i++{
-        isAllowed := true
-        for j := 0; j < len(words[i]); j++{
-        
-            if !slices.Contains(arr, string(words[i][j])){
-                isAllowed = false
-                break
-            }
-        }
-        
-        if isAllowed {
-            count++
-        }
-    }
-    return count
+	count := 0
+loop:
+	for i := 0; i < len(words); i++ {
+
+		for j := 0; j < len(words[i]); j++ {
+			if !strings.Contains(allowed, string(words[i][j])) {
+				continue loop
+			}
+		}
+
+		count++
+
+	}
+	return count
 }
