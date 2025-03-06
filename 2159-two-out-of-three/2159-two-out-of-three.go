741d262eea78fe0aee0f17d85ba8db3cb043a075
Mohamed Ibjas <mtmibjas@gmail.com>
2025-03-06 23:01:24 +0530

    Time: 3 ms (53.85%), Space: 6.2 MB (76.92%) - LeetHub

---
+++ 2159-two-out-of-three/2159-two-out-of-three.go
@@ -0,0 +1,26 @@
+func twoOutOfThree(nums1 []int, nums2 []int, nums3 []int) []int {
+    arr := make([]int, 0)
+    arr = append(arr, nums1...)
+    arr = append(arr, nums3...)
+    arr = append(arr, nums3...)
+     res := make([]int, 0)
+    for _, num := range arr {
+        if slices.Contains(res, num) {
+            continue
+        }
+        c := 0
+        if slices.Contains(nums1, num) {
+            c++
+        }
+        if slices.Contains(nums2, num) {
+            c++
+        }
+        if slices.Contains(nums3, num) {
+            c++
+        }
+        if c > 1 {
+            res = append(res, num)
+        }
+    }
+    return res
+}
