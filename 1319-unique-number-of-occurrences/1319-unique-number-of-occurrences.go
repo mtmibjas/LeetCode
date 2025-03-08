4355944161813625e241d119cfef0f84c655ab6a
Mohamed Ibjas <mtmibjas@gmail.com>
2025-03-08 23:09:58 +0530

    Time: 0 ms (100%), Space: 4.4 MB (21.33%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.go
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.go
@@ -3,12 +3,9 @@ func uniqueOccurrences(arr []int) bool {
     for _, num := range arr {
         m[num]++
     }
-    for i, num := range m {
-        for j, n := range m {
-            if num == n && i != j{
-                return false
-            }
-        }    
+    m2 := make(map[int]struct{})
+    for _, num := range m {
+           m2[num] = struct{}{}
     }
-    return true
+    return len(m)==len(m2)
 }
