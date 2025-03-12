5b05f186fd2e3ee1c00d2f97528ac7490cf89752
Mohamed Ibjas <mtmibjas@gmail.com>
2025-03-12 23:55:53 +0530

    Time: 0 ms (100%), Space: 4.7 MB (65%) - LeetHub

--- 3226-minimum-number-game/3226-minimum-number-game.go
+++ 3226-minimum-number-game/3226-minimum-number-game.go
@@ -1,8 +1,13 @@
 func numberGame(nums []int) []int {
     sort.Ints(nums)
     
-    for i,j := 0,1; j< len(nums); i,j = i+2, j+2 {
-        nums[i], nums[j] = nums[j], nums[i]
+    for i,j := 0,len(nums)-1; i < j; i,j = i+2, j-2 {
+        nums[i], nums[i+1] = nums[i+1], nums[i]
+        if j == i+1 {
+            continue
+        }
+        nums[j], nums[j-1] = nums[j-1], nums[j]
+        
     }
 
     return nums
