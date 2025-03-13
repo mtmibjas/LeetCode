2c61a9e3fc4fb261ec745589e3393fa1c8747e18
Mohamed Ibjas <mtmibjas@gmail.com>
2025-03-13 23:24:59 +0530

    Time: 0 ms (100%), Space: 4.9 MB (84.3%) - LeetHub

--- 3778-transform-array-by-parity/3778-transform-array-by-parity.go
+++ 3778-transform-array-by-parity/3778-transform-array-by-parity.go
@@ -1,16 +1,17 @@
 func transformArray(nums []int) []int {
 	for i, j := 0, len(nums)-1; i <= j; i, j = i+1, j-1 {
-		if nums[i]%2 == 0 {
-			nums[i] = 0
-		} else {
-			nums[i] = 1
-		}
-		if nums[j]%2 == 0 {
-			nums[j] = 0
-		} else {
-			nums[j] = 1
-		}
+		transform(i, nums)
+        transform(j, nums)
+
 	}
 	sort.Ints(nums)
 	return nums
+}
+
+func transform(j int, nums []int) {
+	if nums[j]%2 == 0 {
+		nums[j] = 0
+	} else {
+		nums[j] = 1
+	}
 }
