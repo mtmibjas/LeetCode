4f8288f741f5566244c587b5145efe68c1d82279
Mohamed Ibjas <mtmibjas@gmail.com>
2025-03-18 16:18:27 +0530

    Time: 0 ms (100%), Space: 9.6 MB (42.6%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.go
+++ 0189-rotate-array/0189-rotate-array.go
@@ -1,11 +1,14 @@
-func rotate(nums []int, k int)  {
-   n := len(nums)
-   arr := make([]int, 0)
-   k = k%n
-   arr = append(arr, nums[n-k:]...)
-   arr = append(arr, nums[:n-k]...)
-    for i,num := range arr {
-        nums[i] = num
+func rotate(nums []int, k int) {
+    n := len(nums)
+    if n == 0 || k <= 0 {
+        return
     }
 
+    k = k % n 
+    
+    right := append([]int{}, nums[n-k:]...)
+    left := append([]int{}, nums[:n-k]...)
+
+    copy(nums[:k], right)
+    copy(nums[k:], left)
 }
