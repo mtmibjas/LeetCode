28d70777a231fac343939a23d70d2f86ef9ddc14
Mohamed Ibjas <mtmibjas@gmail.com>
2025-03-18 12:18:28 +0530

    Time: 7 ms (22.04%), Space: 10.5 MB (38.15%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.go
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.go
@@ -1,19 +1,12 @@
 func productExceptSelf(nums []int) []int {
 	n := len(nums)
-	prefix, suffix := make([]int,n), make([]int,n)
-	prefix[0], suffix[n-1] = nums[0], nums[n-1]
+	prefix, suffix := make([]int, n), make([]int, n)
+	prefix[0], suffix[n-1] = 1, 1
 	for i, j := 1, len(nums)-2; i < len(nums); i, j = i+1, j-1 {
-		prefix[i], suffix[j] = prefix[i-1]*nums[i], suffix[j+1]*nums[j]
+		prefix[i], suffix[j] = prefix[i-1]*nums[i-1], suffix[j+1]*nums[j+1]
 	}
-
-    for i, _ := range nums{
-        if i == 0 {
-            nums[i] = suffix[i+1]
-        }else if i == n-1 {
-            nums[i] = prefix[i-1]
-        }else{
-             nums[i] = prefix[i-1] * suffix[i+1]
-        }
-    }
+	for i, _ := range nums {
+		nums[i] = prefix[i] * suffix[i]
+	}
 	return nums
 }
