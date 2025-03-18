8f194c421da7d5841a8846040fa241903695c763
Mohamed Ibjas <mtmibjas@gmail.com>
2025-03-18 14:00:32 +0530

    Time: 2 ms (52.87%), Space: 10.3 MB (39.28%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.go
+++ 0041-first-missing-positive/0041-first-missing-positive.go
@@ -1,22 +1,19 @@
 func firstMissingPositive(nums []int) int {
 	n := len(nums)
-	pNums := []int{}
-	sort.Ints(nums)
-	for i := 0; i < len(nums)-1; i++ {
-		if nums[i] != nums[i+1] && nums[i] > 0 {
-			pNums = append(pNums, nums[i])
-		}
-	}
-	if nums[n-1] > 0 {
-		pNums = append(pNums, nums[n-1])
-	}
-	if len(pNums) == 0 {
-		return 1
-	}
-	for i := 1; i <= pNums[len(pNums)-1]; i++ {
-		if i != pNums[i-1] {
-			return i
-		}
-	}
-	return pNums[len(pNums)-1] + 1
+	
+    for i := 0; i < n; {
+        ci := nums[i]-1
+        if ci < n && ci >= 0 && nums[i] != nums[ci] {
+             nums[i], nums[ci] = nums[ci], nums[i] 
+        }else{
+            i++
+        }
+  
+    }
+    for i := 0; i < n; i++{
+        if nums[i]-1 != i {
+            return i+1
+        }
+    }
+	return n+1
 }
