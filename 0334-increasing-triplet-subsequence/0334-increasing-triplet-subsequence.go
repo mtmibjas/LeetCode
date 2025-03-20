43464dfa05eb7896ccfd2a32eccec8ae4653249a
Mohamed Ibjas <mtmibjas@gmail.com>
2025-03-20 12:12:44 +0530

    Time: 0 ms (100%), Space: 20.8 MB (60.74%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.go
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.go
@@ -4,7 +4,6 @@ func increasingTriplet(nums []int) bool {
 	fmin := math.MaxInt64
 	smin := math.MaxInt64
 	for j := 0; j < len(nums); j++ {
-        fmt.Println(fmin, smin)
 		if nums[j] <= fmin {
            fmin = nums[j] 
         }else if nums[j] <= smin {
