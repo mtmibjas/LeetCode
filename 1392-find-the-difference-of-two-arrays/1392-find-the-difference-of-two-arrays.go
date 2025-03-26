2fd3cd8f3fda69d10a87845d87163e01e06aa406
Mohamed Ibjas <mtmibjas@gmail.com>
2025-03-26 06:08:04 +0530

    Time: 28 ms (6.53%), Space: 9 MB (51.19%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.go
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.go
@@ -1,59 +1,17 @@
 func findDifference(nums1 []int, nums2 []int) [][]int {
-	sort.Ints(nums1)
-	sort.Ints(nums2)
+    arr := make([][]int, 2)
 
-	arr := make([][]int, 2)
-	left := 0
-	right := 0
-	for left < len(nums1) && right < len(nums2) {
-		if nums1[left] == nums2[right] {
-            left++
-            for i := left; i < len(nums1); i++{
-                if nums1[left-1] == nums1[left]{
-                     left++
-                     continue
-                }
-                break
-            }
-			
-			right++
-            for i := right; i < len(nums2); i++{
-                if nums2[right-1] == nums2[right]{
-                     right++
-                     continue
-                }
-                break
-            }
-			continue
-		} else if nums1[left] < nums2[right] {
-			if !slices.Contains(arr[0], nums1[left]) {
-				arr[0] = append(arr[0], nums1[left])
-			}
-			left++
-		} else {
-			if !slices.Contains(arr[1], nums2[right]) {
-				arr[1] = append(arr[1], nums2[right])
-			}
-			right++
-		}
-          fmt.Println(left, right)
-	}
-  
-	if left < len(nums1) {
-		for _, num := range nums1[left:] {
-			if !slices.Contains(arr[0], num) {
-				arr[0] = append(arr[0], num)
-			}
-		}
+    for _, num := range nums1{
+        if !slices.Contains(nums2, num) && !slices.Contains(arr[0], num){
+            arr[0] = append(arr[0], num)
+        }
+    }
 
-	}
-	if right < len(nums2) {
-		for _, num := range nums2[right:] {
-			if !slices.Contains(arr[1], num) {
-				arr[1] = append(arr[1], num)
-			}
-		}
+    for _, num := range nums2{
+        if !slices.Contains(nums1, num) && !slices.Contains(arr[1], num){
+            arr[1] = append(arr[1], num)
+        }
+    }
 
-	}
-	return arr
+    return arr
 }
