03572f249864c4a4e666fc9884c9c39f495e20f6
Mohamed Ibjas <mtmibjas@gmail.com>
2025-03-29 22:08:29 +0530

    Time: 32 ms (75.52%), Space: 9 MB (86.98%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.go
+++ 0575-distribute-candies/0575-distribute-candies.go
@@ -1,15 +1,12 @@
 func distributeCandies(candyType []int) int {
    
     m := make(map[int]struct{})
-
     for _, num := range candyType {
         m[num] = struct{}{}
     }
     l := len(candyType)/2
     lm := len(m)
-    if lm < l {
-        return lm
-    }
-    return l
+  
+    return min(l, lm)
 
 }
