c662cec7fdffef12d5f2355265f0a372329601ff
Mohamed Ibjas <mtmibjas@gmail.com>
2025-04-02 01:20:23 +0530

    Time: 0 ms (100%), Space: 13.3 MB (10.15%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.go
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.go
@@ -1,15 +1,17 @@
 func findMaxAverage(nums []int, k int) float64 {
 
-	for i := 1; i < len(nums); i++ {
-		nums[i] += nums[i-1]
+	sum := 0
+	for i := 0; i < k; i++ {
+		sum += nums[i]
 	}
-	m := math.Inf(-1)
-	nums = append([]int{0}, nums...)
+	mx := sum
 	for i := 1; i < len(nums)-k+1; i++ {
-		avg := float64(nums[i+k-1]-nums[i-1]) / float64(k)
-		if avg > m {
-			m = avg
+		sum -= nums[i-1]
+		sum += nums[i+k-1]
+		if sum > mx {
+			mx = sum
 		}
 	}
-	return m
+
+	return float64(mx) / float64(k)
 }
