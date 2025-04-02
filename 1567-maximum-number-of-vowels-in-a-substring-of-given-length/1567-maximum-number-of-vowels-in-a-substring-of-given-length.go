8c5db4042ec3b41bb228521c0fa9b45dce1885c3
Mohamed Ibjas <mtmibjas@gmail.com>
2025-04-02 07:41:32 +0530

    Time: 22 ms (19.65%), Space: 6.9 MB (14.43%) - LeetHub

---
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.go
@@ -0,0 +1,28 @@
+func maxVowels(s string, k int) int {
+	v := []string{"a", "e", "i", "o", "u"}
+
+	initalCount := 0
+	for i := 0; i < k; i++ {
+		if slices.Contains(v, string(s[i])) {
+			initalCount++
+		}
+	}
+	fmt.Println(initalCount)
+	m := initalCount
+	for i := 1; i < len(s)-k+1; i++ {
+		if slices.Contains(v, string(s[i-1])) {
+			initalCount--
+		}
+		if slices.Contains(v, string(s[i+k-1])) {
+			initalCount++
+		}
+		if m < initalCount {
+			m = initalCount
+		}
+		if m == k {
+			return m
+		}
+	}
+
+	return m
+}
