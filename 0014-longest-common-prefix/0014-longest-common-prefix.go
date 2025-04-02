83dd00cd0003e1974f410abfa26ef67c19aa9de1
Mohamed Ibjas <mtmibjas@gmail.com>
2025-04-02 06:52:23 +0530

    Time: 0 ms (100%), Space: 4.2 MB (99.35%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.go
+++ 0014-longest-common-prefix/0014-longest-common-prefix.go
@@ -7,9 +7,7 @@ func longestCommonPrefix(strs []string) string {
         for len(prefix) > len(strs[i]) || prefix != strs[i][:len(prefix)] && len(prefix) > 0 {
             
            prefix =  prefix[:len(prefix)-1]
-           if len(prefix) <= len(strs[i]) {
-             fmt.Println(strs[i][:len(prefix)], len(prefix))
-           }
+         
         }
 
         if len(prefix) == 0 {
