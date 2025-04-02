c6f1703a76d01ce664cff70faf2972d977275d2a
Mohamed Ibjas <mtmibjas@gmail.com>
2025-04-02 07:43:56 +0530

    Time: 3 ms (72.51%), Space: 6.6 MB (98.13%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.go
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.go
@@ -1,18 +1,18 @@
 func maxVowels(s string, k int) int {
-	v := []string{"a", "e", "i", "o", "u"}
+	v := []byte{'a', 'e', 'i', 'o', 'u'}
 
 	initalCount := 0
 	for i := 0; i < k; i++ {
-		if slices.Contains(v, string(s[i])) {
+		if slices.Contains(v, s[i]) {
 			initalCount++
 		}
 	}
 	m := initalCount
 	for i := 1; i < len(s)-k+1; i++ {
-		if slices.Contains(v, string(s[i-1])) {
+		if slices.Contains(v, s[i-1]) {
 			initalCount--
 		}
-		if slices.Contains(v, string(s[i+k-1])) {
+		if slices.Contains(v, s[i+k-1]) {
 			initalCount++
 		}
 		if m < initalCount {
