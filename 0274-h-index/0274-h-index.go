c5f7a723f283d75ee9dd24202ab364b31b747020
Mohamed Ibjas <mtmibjas@gmail.com>
2025-04-03 10:03:58 +0530

    Time: 0 ms (100%), Space: 4.2 MB (81.59%) - LeetHub

--- 0274-h-index/0274-h-index.go
+++ 0274-h-index/0274-h-index.go
@@ -1,12 +1,10 @@
 func hIndex(citations []int) int {
-    sort.Slice(citations, func(i, j int)bool{
-        return citations[i] > citations[j]
-    })
+    n := len(citations)
+    sort.Ints(citations)
     m := 0
     for i, v := range citations{
-       
-        if v > 0 && v >= i+1 {
-            m = max(m, i+1)
+        if v > 0 && n-i <= v {
+            m = max(m, n-i)
         }
     }
     return m
