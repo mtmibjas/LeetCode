5dc9801d7233debeb58dcc6ad90612ca8d065a59
Mohamed Ibjas <mtmibjas@gmail.com>
2025-04-03 23:45:20 +0530

    Time: 9 ms (7.43%), Space: 7.9 MB (13.11%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.go
@@ -0,0 +1,14 @@
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+
+	nums1 = append(nums1, nums2...)
+
+	sort.Ints(nums1)
+
+	mid := len(nums1) / 2
+	fmt.Println(nums1)
+	if len(nums1)%2 == 1 {
+		return float64(nums1[mid])
+	}
+
+	return float64(nums1[mid-1]+nums1[mid]) / 2
+}
