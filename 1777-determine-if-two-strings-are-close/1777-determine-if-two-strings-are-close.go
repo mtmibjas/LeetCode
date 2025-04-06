794da7d2b3dd696fe36ad971661dfb376619d4a1
Mohamed Ibjas <mtmibjas@gmail.com>
2025-04-06 07:04:40 +0530

    Time: 30 ms (69.07%), Space: 8.8 MB (67.16%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.go
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.go
@@ -2,38 +2,28 @@ func closeStrings(word1 string, word2 string) bool {
 	if len(word1) != len(word2) {
 		return false
 	}
-	arr1 := changeArray(word1)
-	arr2 := changeArray(word2)
-
-	for i, n := range arr1 {
-		if n == 0 {
-			continue
-		}
-        k := arr2[i]
-        if k == 0 {
+	arr1, idx1 := changeArray(word1)
+	arr2, idx2 := changeArray(word2)
+    if len(idx1) != len(idx2) {
+        return false
+    }
+	sort.Ints(arr1[:])
+    sort.Ints(arr2[:])
+    for k,_:= range idx1 {
+        if _, ok := idx2[k]; !ok {
             return false
         }
-		v := slices.Index(arr2[:], n)
-		if v == -1 {
-			return false
-		}
-		arr2[v] = -2
+    }
 
-	}
-	for _, n := range arr2 {
-		if n == 0 || n == -2{
-			continue
-		}
-		return false
-
-	}
-	return true
+    return arr1 == arr2 
 }
 
-func changeArray(str string)[26]int{
+func changeArray(str string)([26]int, map[rune]struct{}){
     arr := [26]int{}
+    m := make(map[rune]struct{})
 	for _, w := range str {
 		arr[w-'a']++
+        m[w-'a']= struct{}{}
 	}
-    return arr
+    return arr, m
 }
