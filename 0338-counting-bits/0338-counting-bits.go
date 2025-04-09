a64d334e65c0b6c589bae158229212d6cc10e625
Mohamed Ibjas <mtmibjas@gmail.com>
2025-04-09 07:07:31 +0530

    Time: 0 ms (100%), Space: 6.4 MB (51.21%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.go
+++ 0338-counting-bits/0338-counting-bits.go
@@ -1,8 +1,8 @@
 func countBits(n int) []int {
-	arr := []int{}
+	arr := make([]int, n+1)
 
     for i := 0; i <= n; i++{
-        arr = append(arr, CountNum(i))
+        arr[i] = CountNum(i)
     }
    
 	return arr
