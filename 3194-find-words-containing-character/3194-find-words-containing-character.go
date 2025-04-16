b657a4f86208974bc83433ceef625fb77c57f42f
Mohamed Ibjas <mtmibjas@gmail.com>
2025-04-16 19:51:45 +0530

    Time: 2 ms (16.24%), Space: 6.5 MB (63.25%) - LeetHub

--- 3194-find-words-containing-character/3194-find-words-containing-character.go
+++ 3194-find-words-containing-character/3194-find-words-containing-character.go
@@ -1,7 +1,14 @@
 func findWordsContaining(words []string, x byte) []int {
     arr := []int{}
     for i, word := range words {
-        if !strings.Contains(word, string(x)){
+        isExist := false    
+        for i,j := 0, len(word)-1; i <= j; i, j = i+1,j-1{
+            if word[i] == x || word[j] == x {
+                isExist = true
+                break
+            }
+        }
+        if !isExist {
             continue
         }
         arr = append(arr,i)
