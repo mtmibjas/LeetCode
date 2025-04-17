f22991b88584a3b7b867ed5407861f0dea09b1e1
Mohamed Ibjas <mtmibjas@gmail.com>
2025-04-17 23:15:28 +0530

    Time: 0 ms (100%), Space: 6 MB (20%) - LeetHub

---
+++ 1651-shuffle-string/1651-shuffle-string.go
@@ -0,0 +1,14 @@
+func restoreString(s string, indices []int) string {
+	m := map[int]rune{}
+	for k, v := range s {
+		m[indices[k]] = v
+	}
+	
+	sort.Ints(indices)
+
+	arr := make([]rune, len(indices))
+	for i := 0; i < len(indices); i++ {
+		arr[i] = m[i]
+	}
+	return string(arr)
+}
