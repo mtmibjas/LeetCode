7eeeb659a6177c8379a526c778685b28756cd546
Mohamed Ibjas <mtmibjas@gmail.com>
2025-04-17 22:53:31 +0530

    Time: 1 ms (77.05%), Space: 7.5 MB (95.08%) - LeetHub

--- 3206-find-common-elements-between-two-arrays/3206-find-common-elements-between-two-arrays.go
+++ 3206-find-common-elements-between-two-arrays/3206-find-common-elements-between-two-arrays.go
@@ -1,15 +1,14 @@
 func findIntersectionValues(nums1 []int, nums2 []int) []int {
-	arr := []int{}
-	c1, c2 := 0, 0
+	arr := []int{0,0}
 	for i, j := 0, len(nums2)-1; i < len(nums1) || j >= 0; i, j = i+1, j-1 {
 		if i < len(nums1) && slices.Contains(nums2, nums1[i]) {
-			c1++
+			arr[0]++
 		}
 		if  j >= 0 && slices.Contains(nums1, nums2[j]) {
-			c2++
+			arr[1]++
 		}
 	}
-	arr = append(arr, c1, c2)
+	//arr = append(arr, c1, c2)
 
 	return arr
 }
