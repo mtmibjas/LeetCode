2ec10100d0dfa68a0d8de1a9bc7b3d5fc0e2b23b
Mohamed Ibjas <mtmibjas@gmail.com>
2025-04-18 21:43:32 +0530

    Time: 0 ms (100%), Space: 6.1 MB (25.44%) - LeetHub

--- 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.go
+++ 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.go
@@ -1,17 +1,11 @@
 func numOfUnplacedFruits(fruits []int, baskets []int) int {
-    c := 0
     for _, f := range fruits {
-        is := false
         for  i := 0; i < len(baskets); i++ {
             if f <= baskets[i] {
-                is = true
                 baskets = append(baskets[:i],baskets[i+1:]...)
                 break
             }
         }    
-        if !is {
-            c++
-        }
     }
-    return c
+    return len(baskets)
 }
