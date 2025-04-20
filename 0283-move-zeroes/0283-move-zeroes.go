99f44cfb7d56f7b896d4e1c06314c167cb16d5aa
Mohamed Ibjas <mtmibjas@gmail.com>
2025-04-20 13:28:45 +0530

    Time: 0 ms (100%), Space: 9 MB (35.56%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.go
+++ 0283-move-zeroes/0283-move-zeroes.go
@@ -1,12 +1,27 @@
-func moveZeroes(nums []int)  {
-   index := 0
-   for i := 0; i < len(nums); i++ {
-      if nums[i] != 0 {
-         nums[index] = nums[i]
-         index++
-      } 
-   }
-    for i := index; i < len(nums); i++ {
-        nums[i] = 0
+// func moveZeroes(nums []int) {
+// 	arr := make([]int, len(nums))
+// 	index := 0
+// 	for _, n := range nums {
+// 		if n > 0 {
+// 			arr[index] = n
+// 			index++
+// 		}
+// 	}
+//     for i, n := range arr {
+//         nums[i] = n
+//     }
+// }
+
+func moveZeroes(nums []int) {
+    index := 0
+    for _, n := range nums {
+        if n != 0 {
+            nums[index] = n
+            index++
+        }
+    }
+    for index < len(nums) {
+        nums[index] = 0
+        index++
     }
 }
