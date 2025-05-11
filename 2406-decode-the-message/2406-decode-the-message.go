bfa484da4e32b6a94ed1d60c90e7917dcc125491
Mohamed Ibjas <mtmibjas@gmail.com>
2025-05-11 15:39:44 +0530

    Time: 0 ms (100%), Space: 4.5 MB (68.97%) - LeetHub

--- 2406-decode-the-message/2406-decode-the-message.go
+++ 2406-decode-the-message/2406-decode-the-message.go
@@ -1,24 +1,19 @@
 func decodeMessage(key string, message string) string {
-	m := make(map[rune]int)
-	index := 0
+	m := make(map[rune]rune)
+	index  := rune(0)
 	for _, e := range key {
-		if _, ok := m[e]; ok  || e == ' '{
+		if _, ok := m[e]; ok || e == 32{
 			continue
 		}
-		
-		m[e] = index
+		m[e] = index + 'a'
 		index++
 	}
-
-	res := ""
-	for _, c := range message {
-        if c == ' ' {
-            res += " "
-            continue
-        }
-		res += string(m[c] + 'a')
+    m[32] = 32
+	res := make([]rune, len(message)) 
+	for i, c := range message {
+		res[i] = m[c] 
 	}
-
-	return res
+    
+	return string(res)
 
 }
